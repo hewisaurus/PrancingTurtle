f6da0605dc7208e91a2ce581a2a23b4f47f753d1
David Hartley <dhartley84@gmail.com>
2018-05-26 22:07:20 +1000

    Live + Prime badges on the index WIP

---
+++ PrancingTurtle/Database/Models/GameEdition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class GameEdition
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Display { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
