9dc9fe0c4592f57f77358040c25f641744c9105c
hewisaurus <dhartley84@gmail.com>
2019-11-08 19:27:49 +1100

    Removed the borked references to the super old syslog logger

---
+++ PrancingTurtle/Logging/NullLogger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public class NullLogger: ILogger
+    {
+        public void Info(string message, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Info(string message, Exception exc, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Debug(string message, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Debug(string message, Exception exc, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Warn(string message, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Warn(string message, Exception exc, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Error(string message, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Error(string message, Exception exc, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Fatal(string message, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Fatal(string message, Exception exc, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Trace(string message, string sourceFilePath = null)
+        {
+            return;
+        }
+
+        public void Trace(string message, Exception exc, string sourceFilePath = null)
+        {
+            return;
+        }
+    }
+}
