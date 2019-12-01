411fe1278eec5d18c92532545e1509fa90f4fee1
hewisaurus <dhartley84@gmail.com>
2019-12-01 20:51:50 +1100

    Added the concept log parser

---
+++ PrancingTurtle/LogParserConcept/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogParserConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
